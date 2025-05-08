73b66f834ba2be5af5410091de360d8fadf83e85
Manojkumar <140330455+Manojkumar6696@users.noreply.github.com>
2025-05-08 12:32:21 +0530

    Add files via upload

---
+++ Test_Locators/locators.py
@@ -0,0 +1,11 @@
+
+#Creatng class to store the page locators
+class Locators:
+    # Login Page
+    username_input_locator = '//*[@id=":r0:"]'  # Xpath
+    password_input_locator = '//*[@id=":r1:"]'
+    login_button_locator = '//button[@class="primary-btn sign-in-pad"]'
+    drop_down_button_locator='//div[@class="profile-click-icon-div"]'
+    pop_up_box='/html/body/div[2]/div[3]/div/div/button'
+    logout_button_locator='//*[@id="root"]/div[2]/div[2]/div[1]/div/div[2]/div/div[2]/div[4]/div[3]'
+
