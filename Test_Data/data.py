304d7845fb022944668d00b756557ac148495975
Manojkumar <140330455+Manojkumar6696@users.noreply.github.com>
2025-05-08 12:30:01 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,8 @@
+#Creatng class to store the page data's
+class Data:
+    url = "https://v2.zenclass.in/login"
+    dashboard_url = "https://v2.zenclass.in/dashboard"
+
+    # login credentials
+    username = "manojsekar.6696@gmail.com"
+    password = "Lucky@96"
